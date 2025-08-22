addd1f2a0541da9deea79167e18e6f7890f1defb
Tristan-hoito <tristanragasa17@gmail.com>
2025-08-22 14:05:53 +0800

    Add files via upload
    update

---
+++ s6/dictionary.py
@@ -0,0 +1,59 @@
+# userinfo = {
+#     "full_name": "Tristan Ragasa",
+#     "age" : 17,
+#     "address" : "Marilao"
+# }
+
+person = dict(name="pogi", age=17, address="Marilao")
+# print("")
+# print(userinfo)
+# print("")
+
+# print("")
+# print(person["name"])
+# print(userinfo["full_name"])
+# print("")
+
+# print(userinfo.get("age"))
+
+# person = {
+#     "full_name" : input("Input your name: "),
+#     "age" : input(int("Input your age: ")),
+#     "address" : input("Input your address: ")
+# }
+
+# person.get("weight")
+# person_weight = person.get("weight")
+
+# person["email"] = "tristanpogi@gmail.com"
+# person["email"] =(input("enter email: "))
+
+# person.update({"email": input("Input your new Email: ")})
+
+# person.pop("email")
+# #removes email
+# del person["age"]
+
+# person.clear()
+
+# print(person)
+
+user = {"name": "Tristan",
+        "age":17, 
+        "pogiba":"POGI",
+        "friends":[
+            {"name": "Mark"},
+            {"name": "Lorenz"},
+            {"name": "Zac"},
+            {"name": "Reynald"}
+                   ]
+        }
+
+for key in user.keys():
+    pass
+
+for value in user.values():
+    pass
+
+for key,value in user.items():
+    print(f"{key}: {value}")
---
+++ s7/wa.py
@@ -0,0 +1,6 @@
+last = input()
+first = input()
+
+full_name = last+" "+first
+
+print(full_name)
